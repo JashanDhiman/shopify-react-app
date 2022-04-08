8bd0b4fefe4b6d9281aa4ca7843f835e85ad6670
jashan <jashandhiman.hulk@gmail.com>
2022-04-08 17:08:45 +0530

    created customer in shopify

--- cors-server/server.js
+++ cors-server/server.js
@@ -1,55 +1,61 @@
 const axios = require("axios");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // routes
 
-app.get("/create-customer", async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.send({ msg: "This has CORS enabled 🎈" });
-  console.log(res);
-  //var data = JSON.stringify({
-  //  customer: {
-  //    first_name: "jas1",
-  //    last_name: "dhiman",
-  //    email: "jas2.lastnmeson@example.com",
-  //    phone: "+19142565013",
-  //    verified_email: true,
-  //    addresses: [
-  //      {
-  //        address1: "123 Oak St",
-  //        city: "Ottawa",
-  //        province: "ON",
-  //        phone: "555-1212",
-  //        zip: "123 ABC",
-  //        last_name: "Lastnameson",
-  //        first_name: "Mother",
-  //        country: "CA",
-  //      },
-  //    ],
-  //  },
-  //});
-
-  //var config = {
-  //  method: "post",
-  //  url: "https://jashan-dev-3.myshopify.com/admin/api/2022-04/customers.json",
-  //  headers: {
-  //    "X-Shopify-Access-Token": "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",
-  //    "Content-Type": "application/json",
-  //  },
-  //  data: data,
-  //};
-
-  //axios(config)
-  //  .then(function (response) {
-  //    console.log(JSON.stringify(response.data));
-  //    console.log("data");
-  //  })
-  //  .catch(function (error) {
-  //    console.log(error.response);
-  //    console.log("error");
-  //  });
-  res.send("Successfully created");
+app.post("/create-customer", (req, res) => {
+  if (req.body) {
+    var config = {
+      method: "post",
+      url: "https://jashan-dev-3.myshopify.com/admin/api/2022-04/customers.json",
+      headers: {
+        "X-Shopify-Access-Token": "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",
+        "Content-Type": "application/json",
+      },
+      data: req.body,
+    };
+    axios(config)
+      .then((response) => {
+        //console.log(response.data);
+        res.send("Successfully Signed-Up");
+      })
+      .catch((error) => {
+        res.status(400).send({
+          message: "Error: Please fill valid email id and phone no!",
+        });
+        //console.log(error.response);
+      });
+  }
 });
+//app.post("/create-customer1", async (req, res) => {
+//  console.log("start");
+//  var config = {
+//    method: "post",
+//    url: "https://jashan-dev-3.myshopify.com/admin/api/2022-04/customers.json",
+//    headers: {
+//      "X-Shopify-Access-Token": "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",
+//      "Content-Type": "application/json",
+//    },
+//    data: customer,
+//  };
+//  axios(config)
+//    .then(function (response) {
+//      console.log(JSON.stringify(response.data));
+//      console.log("data");
+//    })
+//    .catch(function (error) {
+//      console.log(error.response);
+//      console.log("error");
+//    });
+//});
 
 // listening
 
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,64 +2,46 @@ import axios from "axios";
 import React, { useEffect } from "react";
 
 const SignIn = () => {
-  var data = JSON.stringify({
-    customer: {
-      first_name: "jas1",
-      last_name: "dhiman",
-      email: "jas2.lastnmeson@example.com",
-      phone: "+19142565013",
-      verified_email: true,
-      addresses: [
-        {
-          address1: "123 Oak St",
-          city: "Ottawa",
-          province: "ON",
-          phone: "555-1212",
-          zip: "123 ABC",
-          last_name: "Lastnameson",
-          first_name: "Mother",
-          country: "CA",
-        },
-      ],
-    },
-  });
-
-  //var config = {
-  //  method: "post",
-  //  url: "https://jashan-dev-3.myshopify.com/admin/api/2022-04/customers.json",
-  //  headers: {
-  //    "X-Shopify-Access-Token": "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",
-  //    "Content-Type": "application/json",
-  //  },
-  //  data: data,
-  //};
-
-  //axios(config)
-  //  .then(function (response) {
-  //    console.log(JSON.stringify(response.data));
-  //    console.log("data");
-  //  })
-  //  .catch(function (error) {
-  //    console.log(error.response);
-  //    console.log("error");
-  //  });
-
-  //----------------------------------------------------------------------------------
   const makeAPICall = async () => {
-    try {
-      const response = await fetch("http://localhost:8080/create-customer", {
-        mode: "cors",
+    const data = {
+      customer: {
+        first_name: "jas8",
+        last_name: "dhiman",
+        email: "jas8.lastnmeson@example.com",
+        phone: "+19142565019",
+        verified_email: true,
+        addresses: [
+          {
+            address1: "123 Oak St",
+            city: "Ottawa",
+            province: "ON",
+            phone: "555-1212",
+            zip: "123 ABC",
+            last_name: "Lastnameson",
+            first_name: "Mother",
+            country: "CA",
+          },
+        ],
+      },
+    };
+    var config = {
+      method: "post",
+      url: "http://localhost:8080/create-customer",
+      data,
+    };
+
+    await axios(config)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error.response);
       });
-      const data = await response.json();
-      console.log({ data });
-    } catch (e) {
-      console.log(e);
-    }
   };
   useEffect(() => {
     makeAPICall();
   }, []);
-
+  console.log("crate");
   //----------------------------------------------------------------------------------
   return <div>SignIn</div>;
 };
